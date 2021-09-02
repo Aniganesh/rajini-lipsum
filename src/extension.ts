f96854f0cd07fe5862cb57492d41e576ad79d311
aniganesh <aniganesh741@gmail.com>
2021-09-02 13:22:03 +0530

    Improve code remove unnecessary comments

--- src/extension.ts
+++ src/extension.ts
@@ -1,38 +1,18 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import { ExtensionContext, commands, window, SnippetString } from 'vscode';
+import { LineTypes } from './@types';
 import getLines from './getLines';
 import { getInput } from './Helpers/getInput';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	// console.log('Congratulations, your extension "rajini-lipsum" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let test = commands.registerCommand('rajini-lipsum.test', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
+	context.subscriptions.push(commands.registerCommand('rajini-lipsum.test', () => {
 		window.showInformationMessage('Thalaiva!');
-	});
-	let lines = commands.registerCommand('rajini-lipsum.lines', async () => {
+	}));
+	context.subscriptions.push( commands.registerCommand('rajini-lipsum.lines', async () => {
 		const activeEditor = window.activeTextEditor;
 		if (!activeEditor) { return; }
 		const numLines = await getInput('Enter number of lines');
-		activeEditor.insertSnippet(new SnippetString(getLines(Number.parseInt(numLines || ''), 'punch')));
-		// activeEditor.edit((editBuilder) => {
-		// 	const pos = activeEditor.selection.active;
-		// 	editBuilder.insert(pos, getLines(Number.parseInt(numLines || ''), 'punch'));
-		// });
-	});
-	context.subscriptions.push(test);
-	context.subscriptions.push(lines);
+		activeEditor.insertSnippet(new SnippetString(getLines(Number.parseInt(numLines || ''), LineTypes.punch))); 
+	}));
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() { }
